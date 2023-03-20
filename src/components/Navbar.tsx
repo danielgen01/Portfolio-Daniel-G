46a50f333873e03e4e84a65627049003e670d6da
Daniel <gendik38@gmail.com>
2023-03-20 14:50:49 +0100

    add "Home" button

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,7 @@ const Navbar:React.FC = () => {
     <>
     <div className="navbar w-auto h-20  flex justify-between items-center bg-[#1d2021]">
         <div className="logo ml-5 lg:ml-20 text-white font-medium  text-sm lg:text-xl  bg-gradient-to-r from-sky-500 to to-indigo-500 px-2 py-3 rounded-md">
-            {/* <h2>Daniel Adrian Gendik</h2> */}
+            <a href="#">Home</a>
         </div>
 
         <div className="nav h-full mr-20">
