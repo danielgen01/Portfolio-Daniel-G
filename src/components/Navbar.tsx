47d39f33fc4eab10a2ec132cdb8b13aa94253976
Daniel <gendik38@gmail.com>
2023-03-21 09:43:40 +0100

    remove logo div

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,10 +4,8 @@ import { Link, animateScroll as scroll } from "react-scroll"
 const Navbar: React.FC = () => {
   return (
     <>
-      <div className="navbar w-auto h-20  flex justify-between items-center bg-[#1d2021]">
-        <div className="logo ml-5 lg:ml-20 text-white font-medium  text-sm lg:text-xl  bg-gradient-to-r from-sky-500 to to-indigo-500 px-2 py-3 rounded-md">
-          <a href="#">Home</a>
-        </div>
+      <div className="navbar w-auto h-20  flex justify-end items-center bg-[#1d2021]">
+       
 
         <div className="nav h-full mr-20">
           <nav className="flex gap-5  text-white text-lg font-medium h-full w-full items-center">
