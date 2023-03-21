271558cc866a5b6db115f35d0aa307081d5f5b65
Daniel <gendik38@gmail.com>
2023-03-21 09:04:16 +0100

    add skew line

--- src/components/ProjectsBanner.tsx
+++ src/components/ProjectsBanner.tsx
@@ -1,22 +1,26 @@
-import { Project1 } from './project-1';
-import { Project2 } from './project-2';
+import { Project1 } from "./project-1"
+import { Project2 } from "./project-2"
 import React from "react"
 
 const ProjectsBanner: React.FC = () => {
   return (
-    <div
-      className="projects-container flex flex-col justify-center h-auto w-screen gap-10"
-      id="projects-container"
-    >
-      <div className="projects-headline">
-        <h1 className="font-bold text-5xl text-center mt-5 text-white">
-          Projects
-        </h1>
+    <>
+      <div
+        className="projects-container flex flex-col justify-center h-auto w-screen gap-10"
+        id="projects-container"
+      >
+        <div className="projects-headline">
+          <h1 className="font-bold text-5xl text-center mt-5 text-white">
+            Projects
+          </h1>
+        </div>
+
+        <Project1 />
+        <Project2 />
       </div>
 
-     <Project1 />
-     <Project2 />
-    </div>
+      <div className="sketch h-2 bg-gradient-to-r from-sky-500 to-indigo-500 mt-10 -skew-y-2 w-full"></div>
+    </>
   )
 }
 
