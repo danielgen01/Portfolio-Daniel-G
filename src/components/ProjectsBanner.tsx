1f1b16dd11c85ca6c99942fbd37ecb394977406e
Daniel <gendik38@gmail.com>
2023-03-21 08:51:31 +0100

    add first project to site

--- src/components/ProjectsBanner.tsx
+++ src/components/ProjectsBanner.tsx
@@ -13,31 +13,24 @@ const ProjectsBanner: React.FC = () => {
           Projects
         </h1>
       </div>
-      <div className="grid-layout grid grid-cols-1 lg:grid-cols-2 gap-5 mt-5 mx-10 lg:mx-20">
-        <div
-          className="project-container bg-gradient-to-r from-indigo-500 to-sky-500
-         rounded-3xl h-96 shadow-xl text-white flex flex-col items-center hover:opacity-90"
-        >
-          <h1 className="text-4xl font-bold uppercase text-center">
-            Kanbanboard
-          </h1>
-          <div className="flex flex-col lg:flex-row justify-start w-full lg:grid-cols-2 lg:h-full">
-            <img src={Kanbanscreen} className="h-3/4" alt="" />
-            <p>Demo</p>
-          </div>
-        </div>
 
-        <div
-          className="project-container bg-gradient-to-r from-sky-500 to-indigo-500
-         rounded-3xl h-96 shadow-xl  text-white flex flex-col items-center hover:opacity-90
-        "
-        >
-          <h1 className="text-4xl font-bold uppercase text-center">
-            E-Commerce store
-          </h1>
-          <div className="flex flex-col lg:flex-row justify-start w-full lg:grid-cols-2 lg:h-full">
-            <img src={Ecommercescreen} className="h-3/4" alt="" />
-            <p>Demo</p>
+      <div className="grid-layout grid grid-cols-1 lg:grid-cols-2 mx-10 mt-10 bg-gray-500/10 rounded-md p-5">
+        <div className="grid-item-image-container">
+          <img src={Kanbanscreen} alt="Kanbanscreen"/>
+        </div>
+        <div className="project-description text-white flex flex-col items-center gap-5 justify-between">
+          <h1 className="font-semibold text-3xl">Kanbanboard App</h1>
+          <h2 className="text-2xl text-center lg:mx-20">
+            I built an Kanbanboard for tracking my workflow and to get an
+            overview of my tasks
+          </h2>
+          <div className="buttons flex gap-5">
+            <button className="bg-gradient-to-r from-sky-500 to-indigo-500 px-5 py-2 rounded-md font-medium hover:opacity-75">
+              View Livedemo
+            </button>
+            <button className="bg-gradient-to-r from-indigo-500 to-sky-500 px-5 py-2 rounded-md font-medium hover:opacity-75">
+              Sourcecode
+            </button>
           </div>
         </div>
       </div>
