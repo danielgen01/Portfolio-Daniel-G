7e7bff8fd5c0af6fdb4cf1b2932443ab9990e227
Daniel <gendik38@gmail.com>
2023-03-23 18:32:42 +0100

    add a div for social media icons

--- src/components/MainBanner.tsx
+++ src/components/MainBanner.tsx
@@ -2,13 +2,15 @@ import TechnologiesBanner from "./TechnologiesBanner"
 import React from "react"
 import Bewerbungsbild from "../assets/images/BewerbungsbildCartoon.png"
 import CV from  "../assets/images/Lebenslauf Daniel Adrian Gendik.pdf"
+import { BsGit, BsGithub,BsLinkedin } from "react-icons/bs"
+
 
 
 const MainBanner: React.FC = () => {
   return (
     <div className="main-banner text-white selection:bg-sky-300"id="main-banner">
-      <div className="grid grid-cols-1 lg:grid-cols-2 justify-between mt-12 lg:items-center lg:mt-0">
-        <div className="description flex flex-col gap-5  text-center lg:text-left lg:items-start items-center">
+      <div className="grid grid-cols-1 lg:grid-cols-9 justify-between mt-12 lg:items-center lg:mt-0 mx-10">
+        <div className="description flex flex-col gap-5  text-center lg:text-left lg:items-start items-center col-span-4">
           <h1 className="font-bold text-2xl lg:text-6xl">Hi, I'm Daniel</h1>
           <h2 className="font-normal text-xl lg:text-4xl">
             My name is Daniel Gendik and I'm a 22 year old (Web) Frontend
@@ -16,7 +18,7 @@ const MainBanner: React.FC = () => {
           </h2>
         </div>
 
-        <div className="picture-ctn w-full flex flex-col items-center justify-center ">
+        <div className="picture-ctn w-full flex flex-col items-center justify-center col-span-4">
           <div className="image-ctn flex justify-center">
             <img
               src={Bewerbungsbild}
@@ -33,6 +35,11 @@ const MainBanner: React.FC = () => {
               Checkout my CV
             </a>
           </div>
+        </div>
+        <div className=" h-full flex lg:flex-col justify-center items-end w-full gap-5 mt-5 lg:mt-0 col-span-1">
+          <a href="https://github.com/danielgen01" target={"_blank"}><BsGithub className="text-white text-5xl hover:text-[#171515] duration-200"/></a>
+          <a href="https://www.linkedin.com/in/daniel-gendik-b7b277254/" target={"_blank"}><BsLinkedin className="text-white text-5xl hover:text-[#0072b1] duration-200"/></a>
+          
         </div>
       </div>
 
