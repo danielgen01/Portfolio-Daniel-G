1532241cd61f55a31ca39573659d1edb1f6ad1d7
Daniel <gendik38@gmail.com>
2023-03-30 09:20:13 +0200

    put backgroundColor property in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState,useEffect } from "react"
+import { useState, useEffect } from "react"
 import "./App.css"
 import Navbar from "./components/Navbar"
 import MainBanner from "./components/MainBanner"
@@ -6,13 +6,12 @@ import ProjectsBanner from "./components/ProjectsBanner"
 import TechnologiesBanner from "./components/TechnologiesBanner"
 import AboutMe from "./components/AboutMe"
 import Contact from "./components/Contact"
-import {BsArrowUpCircle} from "react-icons/bs"
+import { BsArrowUpCircle } from "react-icons/bs"
 import { Link, animateScroll as scroll } from "react-scroll"
 
-
 function App() {
-  const [isNavBarOpen,setIsNavBarOpen] = useState<boolean>(false)
-  const [isArrowVisible,setIsArrowVisible] = useState<boolean>(false)
+  const [isNavBarOpen, setIsNavBarOpen] = useState<boolean>(false)
+  const [isArrowVisible, setIsArrowVisible] = useState<boolean>(false)
   const deactivateScroll = () => {
     if (!isNavBarOpen) {
       document.body.style.overflow = "hidden"
@@ -24,43 +23,47 @@ function App() {
   const toggleNavBar = () => {
     setIsNavBarOpen(!isNavBarOpen)
     deactivateScroll()
-
   }
 
   useEffect(() => {
     const handleScroll = () => {
       if (window.pageYOffset > 50) {
-        setIsArrowVisible(true);
+        setIsArrowVisible(true)
       } else {
-        setIsArrowVisible(false);
+        setIsArrowVisible(false)
       }
-    };
+    }
 
-    window.addEventListener("scroll", handleScroll);
+    window.addEventListener("scroll", handleScroll)
 
     return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
+      window.removeEventListener("scroll", handleScroll)
+    }
+  }, [])
   return (
-    <div className="APP w-screen">
-      <Navbar isNavBarOpen={isNavBarOpen} setIsNavBarOpen={setIsNavBarOpen} toggleNavBar={toggleNavBar}/>
+    <div className="APP w-screen bg-[rgba(0,0,5)]">
+      <Navbar
+        isNavBarOpen={isNavBarOpen}
+        setIsNavBarOpen={setIsNavBarOpen}
+        toggleNavBar={toggleNavBar}
+      />
       <MainBanner />
-      <TechnologiesBanner     />
+      <TechnologiesBanner />
       <ProjectsBanner />
       <AboutMe />
       <Contact />
       <Link
-              to="main-banner"
-              spy={true}
-              smooth={true}
-              offset={-70}
-              duration={500}
-              activeClass="active"
-              className="hover:text-sky-500 duration-200 cursor-pointer"
-              
-           style={{display: isArrowVisible? "flex" :"none"}} >
-      <BsArrowUpCircle className="fixed text-sky-500 text-5xl bottom-10 right-10"/></Link>
+        to="main-banner"
+        spy={true}
+        smooth={true}
+        offset={-70}
+        duration={500}
+        activeClass="active"
+        className="hover:text-sky-500 duration-200 cursor-pointer"
+        style={{ display: isArrowVisible ? "flex" : "none" }}
+      >
+        <BsArrowUpCircle className="fixed text-sky-500 text-5xl bottom-10 right-10" />
+      </Link>
     </div>
   )
 }
